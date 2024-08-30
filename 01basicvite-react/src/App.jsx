b7686e03b6d14bcbc5a73c6e3aacfa87f40fc33e
akshayjha82 <akshayjha8263@gmail.com>
2024-08-30 23:21:16 +0530

    custom react done

--- 01basicvite-react/src/App.jsx
+++ 01basicvite-react/src/App.jsx
@@ -1,10 +1,10 @@
 import Chai from './chai'
 
 function App() {
-
+const user = 'APPU'
   return (
     <>
-      <h1>Chai aur Vite | Akshay </h1>
+      <h1>Chai aur Vite | {user} </h1>  {/* here we used a parameter created in function scope */}
       <Chai></Chai>
     </>
   )
--- 01basicvite-react/src/main.jsx
+++ 01basicvite-react/src/main.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp() {
+  return (
+    <div>
+      <h1>Custom Function</h1>
+      <p>(A + B)^2 = A^2 + B^2 +2*A*B</p>
+    </div>
+  )
+}   // we try returning the html in the same file in whixh it is being rendered 
+
+const AnotherElement = (
+  <div>Custom another element </div>
+) // we try directly returning the html instead of in form of function 
+
+// whenevr we return a html the react parses that into an object which is understandable by it .So what if we directly send a object will it execute => NO it will not as we don't know what it accepts in object the names of keys thus we cannot return an object .
+
+// We can create an element via createElement which is a property provided by react which creates a html element and we can use it the same way we use jsx .
+
+const anotherUser = 'Jai Mata Di'
+
+const CreatedElemnt = React.createElement(
+  'a',
+  {href : 'https://google.com', target : '_blank'},
+  'Click me to visit google',
+  anotherUser // we just added a parameter in the children
+)
+
 ReactDom.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    //  <Myapp />   // It can also be executed as `Myapp()`
+    //  <AnotherElement/> // this way it will not executed 
+    // AnotherElement  // Now it can be executed
+    // CreatedElemnt 
     <App />
-  </React.StrictMode>,
 )
